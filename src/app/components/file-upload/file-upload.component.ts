4d51c8485f0bb183fae826d7468ec43c352460e2
Fred <dfredmota3873@gmail.com>
2023-09-06 16:05:09 -0300

    estrutura  inicial + upload ans snackbar

---
+++ src/app/components/file-upload/file-upload.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FileUploadService } from 'src/app/services/file-upload.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent implements OnInit {
+  currentFile?: File;
+  progress = 0;
+  message = '';
+
+  fileName = 'Selecione o Arquivo';
+  fileInfos?: Observable<any>;
+
+  constructor(private uploadService: FileUploadService,
+    private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.fileInfos = this.uploadService.getFiles();
+  }
+
+  selectFile(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      const file: File = event.target.files[0];
+      this.currentFile = file;
+      this.fileName = this.currentFile.name;
+    } else {
+      this.fileName = 'Selecione o Arquivo';
+    }
+  }
+
+  upload(): void {
+    this.progress = 0;
+    this.message = "";
+
+    if (this.currentFile) {
+      this.uploadService.upload(this.currentFile).subscribe(
+        (event: any) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progress = Math.round(100 * event.loaded / event.total);
+          } else if (event instanceof HttpResponse) {
+            this.message = event.body.message;
+            this.snackBar.open("Upload de Arquivo realizado com sucesso", "Sucesso", { duration: 5000 });
+            //this.fileInfos = this.uploadService.getFiles();
+          }
+        },
+        (err: any) => {
+          console.log(err);
+          this.progress = 0;
+
+          if (err.error && err.error.message) {
+            this.message = err.error.message;
+          } else {
+            this.message = 'Não foi possível fazer o upload';
+          }
+
+          this.currentFile = undefined;
+        });
+    }
+
+  }
+}
